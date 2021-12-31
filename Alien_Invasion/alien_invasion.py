f91db888c7cca52d17fe1403b932235e204ac710
Antony Lodge <71594095+lodgeant@users.noreply.github.com>
2021-12-31 15:05:18 +0000

    Updated code for: #1: Generate base game footprint

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -1,17 +1,20 @@
 import sys
 import pygame
 
+from settings import Settings
+
+# Global variables
+GAME_VERSION = "v0.1"
+
+
+# Main game loop
 def run_game():
-    """ Init game and create a screen object """
-
+    """ Init game, settings and screen object """
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion v0.1")
-
-    # Set the background color
-    bg_color = (230, 230, 230)
-
-
+    ai_settings = Settings()    
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion " + GAME_VERSION)
+    
     # Start the main loop for the game
     while True:
 
@@ -21,7 +24,7 @@ def run_game():
                 sys.exit()
 
         # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
