5958904f5b703521496a10b11104d1f253d10528
Antony Lodge <71594095+lodgeant@users.noreply.github.com>
2021-12-31 11:15:27 +0000

    Final code for v1.0

--- Alien_Invasion/game_functions.py
+++ Alien_Invasion/game_functions.py
@@ -29,21 +29,21 @@ def check_keyup_events(event, ship):
         ship.moving_right = False
     if event.key == pygame.K_LEFT:                    
         ship.moving_left = False
-   
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+  
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     """ Respond to key presses and mouse events """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()                            
-        elif event.type == pygame.KEYDOWN:                
-            check_keydown_events(event, ai_settings, screen, ship, bullets)                                            
-        elif event.type == pygame.KEYUP:                
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """ Start a new game when the player clicks Play """
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
@@ -57,6 +57,12 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         # Reset the game stats
         stats.reset_stats()
         stats.game_active = True
+        
+        # Reset the scoreboard images
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
+        sb.prep_ships()
 
         # Empty the list of aliens and bullets
         aliens.empty()
@@ -65,7 +71,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         # Create a new fleet and center the ship
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
-        
+   
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """ Update images on the screen and flip to the new screen """
     # Redraw the screen during each pass through the loop
@@ -108,16 +114,20 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
-        #stats.score += ai_settings.alien_points
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
         check_high_score(stats, sb)
 
     if len(aliens) == 0:
-        # Destroy existing bullets, speed up game and create new fleet
+        # If the entire fleet is destroyed, start a new level
         bullets.empty()
         ai_settings.increase_speed()
+        
+        # Increase level
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
         
 
@@ -169,13 +179,16 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
    
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """ Respond to ship being hit by alien """
     
     if stats.ships_left > 0:
     
         # Decrement ships left
         stats.ships_left -= 1
+
+        # Update scoreboard
+        sb.prep_ships()
 
         # Empty the list of aliens and bullets
         aliens.empty()
@@ -192,29 +205,27 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """ Check if any aliens have reached the bottom of the screen """
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # Treat this the same as if the ship got hit
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
             break
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """ Check if the fleet is at an edge, then update the positions of all aliens in the fleet """
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # Look for alien-ship collisions
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
     # Look for aliens hitting the bottom of the screen
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
-
-
-
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
+    
 def check_high_score(stats, sb):
     """ Check to see if there's a new high score """
     if stats.score > stats.high_score:
--- Alien_Invasion/game_stats.py
+++ Alien_Invasion/game_stats.py
@@ -17,3 +17,4 @@ class GameStats():
         """ Init stats that can change during game """
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+        self.level = 1
